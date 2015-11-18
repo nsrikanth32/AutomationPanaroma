4eb3416073d5bcffd4f60cfbac31b5196be0ffdf
Srikanth Nellore <srikanth.nellore@cashconverters.com>
2015-11-18 16:41:48 +0800

    Sample project to showcase how to use xUnit to write tests

---
+++ xUnit_Getting_Started/Hello_xUnit/Hello_xUnit/TestClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hello_xUnit
+{
+    public class TestClass
+    {
+
+        // Facts are tests which are always true. They test invariant conditions. 
+        [Fact]
+        public void HappyTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void UnHappyTest()
+        {
+            Assert.Equal(3, Add(2, 2));
+        }
+
+        int Add(int p1, int p2)
+        {
+            return p1 + p2;
+        }
+    }
+}
---
+++ xUnit_Getting_Started/Hello_xUnit/Hello_xUnit/TheoryTestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hello_xUnit
+{
+    public class TheoryTestClass
+    {
+        //Theories are tests which are only true for a particular set of data
+
+       [Theory]
+       [InlineData(3)]
+       [InlineData(4)]
+       [InlineData(5)]
+       [InlineData(6)]
+       public void TestTheory(int i)
+       {
+           Assert.True(IsOdd(i));
+       }
+
+       bool IsOdd(int i)
+       {
+           return i % 2 == 1;
+       }
+    }
+}
